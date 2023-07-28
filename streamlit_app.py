c4bac00908f5b01bb5f50c7d7d3c555da8c8703f
strangerstork <vlad@stork.network>
2023-07-28 09:51:23 -0700

    initial version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,57 @@
 from collections import namedtuple
 import altair as alt
 import math
+import numpy as np
 import pandas as pd
 import streamlit as st
 
+import json
+
+@st.cache_data(ttl=60)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv")
+    return pd.read_csv(csv_url, on_bad_lines='skip')
+
+
+FUNCTIONS = ["HR", "Sales", "Marketing", "Finance", "Operations", "IT", "Legal", "Other"]
+
+st.title('TechCrunch Articles')
+# open all_articles_v2.json
+
+# with open('all_articles_v2.json') as f:
+#     data = json.load(f)
+
+# convert data, which is a list of dictionaries, to a dataframe
+# but, exclude the 'content' column
+
+# df = pd.DataFrame(data).drop(columns=['content', 'link'])
+df = load_data(st.secrets["public_gsheets_url"])
+
+option = st.sidebar.selectbox("Your Function", FUNCTIONS)
+additiona_info = st.sidebar.text_input("More About You", key="more_details")
+st.table(df)
+
+
+
 """
-# Welcome to Streamlit!
+# "database - articles"
+# https://docs.google.com/spreadsheets/d/1GnAgAxrPD1KK_5m9pjnEXX7l4m5p1owHTSdRU96jvlQ/edit?usp=sharing
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# If not already done, store the value of all_articles_v2.json in a database (with ids)
+# Create a table called persona-article-match with the following columns:
+# - id (primary key)
+# - persona_id (int)
+# - article_id (int)
+# - article_score (float)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Create a table called persona with the following columns:
+# - id (primary key)
+# - function (string)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+# create a function that takes in N articles at a time and a persona id
+# and returns each article with a score using OpenAI (use json as the tool, or just do this in colab)
+
+# create a function that takes in a detailed person description + persona and a list of article IDs
+# and rewrites each article for the user
+# ensure all data is cached
 """
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
